01ad95419e54496122acd70e94a004c25239e651
Mattlotus <mattlotus91@gmail.com>
2023-10-04 13:22:07 -0400

    navBar classes added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,13 @@ function NavBar() {
   return (
     <div>
         <div>
+            <div className='navBar'>
             <img src="" alt="" />
             <input type="text" class="search"> </input>
             <img src="" alt="" className="icon"/>
             <img src="" alt="" className="icon"/>
             <img src="" alt="" className="icon" />
+            </div>
         </div>
     </div>
   )
