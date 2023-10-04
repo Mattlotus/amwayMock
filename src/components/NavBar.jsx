c57677faef14b9978e0e36aa95dde9c3cd7e627f
Mattlotus <mattlotus91@gmail.com>
2023-10-04 13:19:11 -0400

    new navBar edits

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+        <div>
+            <img src="" alt="" />
+            <input type="text" class="search"> </input>
+            <img src="" alt="" className="icon"/>
+            <img src="" alt="" className="icon"/>
+            <img src="" alt="" className="icon" />
+        </div>
+    </div>
   )
 }
 
